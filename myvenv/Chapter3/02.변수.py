c3a512f0c5315ec2a2bec3cb2cd7d4f905c41a54
hyun3607 <iou3607@gmail.com>
2022-07-05 16:22:34 +0900

    variable

---
+++ myvenv/Chapter3/02.변수.py
@@ -0,0 +1,17 @@
+# 변수
+# - 변수이름 = 데이터
+# - 데이터를 저장할 공간
+# - 언제든지 데이터를 변경할 수 있다.
+# 데이터를 표현할 수 있는 이름으로 짓는다.
+
+name = "마스터이"
+level = 5
+health = 800
+attack = 90
+print(name, level, health, attack)
+
+# 변수에 저장된 데이터를 변경하기
+level = level + 1
+health = health + 50
+attack = attack + 10
+print(name, level, health, attack)
