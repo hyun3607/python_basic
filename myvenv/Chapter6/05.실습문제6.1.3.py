5f9e5314ff6b9a9eb7f49dd9041e707f92a5b2d3
hyun3607 <iou3607@gmail.com>
2022-07-06 14:19:22 +0900

    tuple

--- myvenv/Chapter6/05.실습문제6.1.3.py
+++ myvenv/Chapter6/05.실습문제6.1.3.py
@@ -13,8 +13,8 @@ while True:
     if count > 5:
         break
     random_number = get_random_number()
-    if random_number not in lotto_num
-    lotto_num.append(random_number)
+    if random_number not in lotto_num:
+        lotto_num.append(random_number)
     count += 1
 lotto_num.sort()
 for num in lotto_num:
