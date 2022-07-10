94d211d62af2bf962496eb9a01a8f879837619f3
hyun3607 <iou3607@gmail.com>
2022-07-10 16:30:40 +0900

    arr

--- part7/shrinkers/shortener/views.py
+++ part7/shrinkers/shortener/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from shortener.models import Users
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+
+def index(request):
+    user = Users.objects.filter(username="admin").first()
+    email = user.email if user else "Anonymous User!"
+    if request.user.is_authenticated is False:
+        email = "Anonymous User!"
+    print(email)
+    return render(request, "base.html", {"welcome_msg": "Hello FastCampus!"})
+
+
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+
+        return JsonResponse(dict(msg="You just reached with Post Method!"))
--- part7/shrinkers/shrinkers/urls.py
+++ part7/shrinkers/shrinkers/urls.py
@@ -1,7 +1,7 @@
 """shrinkers URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
+from django.conf.urls import include
+from shortener.views import index
+from shortener.views import index, get_user
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("__debug__/", include(debug_toolbar.urls)),  # Django Debug Tool
+    path("", index, name="index"),
+    path("get_user/<int:user_id>", get_user),
 ]
