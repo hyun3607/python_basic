6214ef8068cd7e71fbb3a148b1ed570cd2208333
hyun3607 <iou3607@gmail.com>
2022-07-11 17:40:11 +0900

    SQL

---
+++ part2/python_advanced/myvenv/Chapter05/03.SELECT.py
@@ -0,0 +1,22 @@
+# 모듈 추가
+import sqlite3
+
+# 데이터베이스 열기
+conn = sqlite3.connect('C:/Users/hyun/python/python_basic/part2/python_advanced/myvenv/Chapter05/SQL_DDL.db')
+
+# 커서 생성
+cur = conn.cursor()
+
+# SQL 명령 작성
+SELECT_SQL = "SELECT * FROM item LIMIT 2;"
+
+# SQL 명령 실행
+cur.execute(SELECT_SQL)
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+# 데이터베이스 닫기
+conn.close()
+    
